cded33dc9bb67233baaff45893016f041fb5f041
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-02-14 22:32:47 +0500

    comments

---
+++ Comments.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // This is a single-line comment
+
+    /* 
+       This is a 
+       multi-line comment 
+    */
+
+    cout<<"Understanding Comments!"; 
+    return 0;
+}
+
