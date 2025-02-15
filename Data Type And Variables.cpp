9482d5a3132c0b5eb8132054b5807ecd7d2419d6
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-02-15 12:13:25 +0500

    Variables

---
+++ Data Type And Variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age=25;
+    float price=99.99;
+    char grade='A';
+    bool isPassed=true;
+    cout<<"Age: "<<age<<"\n";
+    cout<<"Price: "<<price<<"\n";
+    cout<<"Grade: "<<grade<<"\n";
+    cout<<"Passed: "<<isPassed<<"\n";
+    return 0;
+}
+
