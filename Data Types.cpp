3768f258b195d7a7c30c01f2f7c2551d6ca833e2
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-02-16 18:07:14 +0500

    Data Types

---
+++ Data Types.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int num=10;
+    double pi=3.1415;
+    char letter='A';
+    bool isCplusplusFun=true;
+    string name="Tulika Singh";
+    cout<<"Integer: "<<num<<"\n";
+    cout<<"Double: "<<pi<<"\n";
+    cout<<"Character: "<<letter<<"\n";
+    cout<<"Boolean: "<<isCplusplusFun<<"\n";
+    cout<<"String: "<<name<<"\n";
+    return 0;
+}
+
