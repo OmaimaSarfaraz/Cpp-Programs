b1226ebc89525e49affc5c5e8aa62f11bdac4bac
OmaimaSarfaraz <aimasiddique.2006@gmail.com>
2025-03-01 12:13:03 +0500

    Recursive Function

---
+++ Function Recursion 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int factorial(int n)
+{
+    return (n==0)?1:n*factorial(n-1);
+}
+int main()
+{
+    cout<<"Factorial of 5: "<<factorial(5);
+    return 0;
+}
